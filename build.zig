c83018005ac368f0ae645fdb5feda8e43d6bd70e
Riccardo Torreggiani <riccardo.torreggiani@gmail.com>
2025-03-13 21:51:50 +0100

    change github CI to build optimization mode release small

--- build.zig
+++ build.zig
@@ -1,11 +1,8 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{}); // e.g.: std.builtin.OptimizeMode.ReleaseSmall
 
     const exe = b.addExecutable(.{
         .name = "zig-wol",
