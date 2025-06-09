32713f211162fbb012244500750953a1a60c523f
Riccardo Torreggiani <riccardo.torreggiani@gmail.com>
2025-06-09 16:47:47 +0200

    install powershell script always use .tar.gz

--- install/install-latest-on-linux.sh
+++ install/install-latest-on-linux.sh
@@ -3,13 +3,9 @@
 repo="rktr1998/zig-wol"
 apiUrl="https://api.github.com/repos/$repo/releases/latest"
 
-# Fetch latest release information from GitHub API
 latestRelease=$(curl -s $apiUrl)
-
-# Extract the tag name from the JSON response
 latestTag=$(echo $latestRelease | grep -oP '"tag_name": "\K[^"]+')
 
-# Detect architecture
 arch=$(uname -m)
 if [ "$arch" == "x86_64" ]; then
     arch="x86_64"
@@ -20,22 +16,16 @@ else
     exit 1
 fi
 
-# OS is Linux
 os="linux"
-
-# Preferred archive format
 extension="tar.gz"
 
-# Construct asset name and download URL
 assetName="zig-wol-$arch-$os.$extension"
 downloadUrl="https://github.com/$repo/releases/download/$latestTag/$assetName"
 
-# Define paths
 homeDir=$HOME
 installDir="$homeDir/.zig-wol"
 tempTar="$installDir/$assetName"
 
-# If install directory exists already ask user for fresh install
 if [ -d "$installDir" ]; then
     echo "Existing installation detected in: $installDir"
     read -p "Do you want to proceed with a fresh install? Existing configurations will be lost. (y/n): " response
@@ -48,35 +38,26 @@ if [ -d "$installDir" ]; then
     rm -rf "$installDir"
 fi
 
-# Create install directory
 echo "Creating installation directory: $installDir"
 mkdir -p "$installDir"
 
-# Download the file
 echo "Detected architecture: $arch"
 echo "Downloading latest release ($latestTag) from $downloadUrl..."
 curl -L $downloadUrl -o "$tempTar"
 
-# Extract the tar file
 echo "Extracting archive..."
 tar -xzvf "$tempTar" -C "$installDir" --strip-components=1
 
-# Clean up tar file
 rm "$tempTar"
 
 echo "Installation completed! Files are in: $installDir"
 
-# Check if path is already in PATH
 if ! echo $PATH | grep -q "$installDir"; then
-    # Only ask the user if the path is not already in PATH
     read -p "Do you want to add $installDir to your PATH? (y/n): " response
     if [ "$response" == "y" ]; then
-        # Append new path with a comment in .bashrc
         echo "Adding $installDir to PATH..."
 
-        # Check if the line already exists in .bashrc
         if ! grep -q "$installDir" "$HOME/.bashrc"; then
-            # Insert a comment before adding the path
             echo -e "# zig-wol" >> "$HOME/.bashrc"
             echo "export PATH=\"$installDir:\$PATH\"" >> "$HOME/.bashrc"
             source "$HOME/.bashrc"
--- install/install-latest-on-windows.ps1
+++ install/install-latest-on-windows.ps1
@@ -1,11 +1,9 @@
 $repo = "rktr1998/zig-wol"
 $apiUrl = "https://api.github.com/repos/$repo/releases/latest"
 
-# Fetch latest release information from GitHub API
 $latestRelease = Invoke-RestMethod -Uri $apiUrl
 $latestTag = $latestRelease.tag_name
 
-# Detect architecture
 $arch = $env:PROCESSOR_ARCHITECTURE
 if ($arch -eq "AMD64") {
     $arch = "x86_64"
@@ -16,22 +14,16 @@ if ($arch -eq "AMD64") {
     exit 1
 }
 
-# OS is Windows since it's PowerShell
 $os = "windows"
+$extension = "tar.gz"
 
-# Preferred archive format
-$extension = "zip"
-
-# Construct asset name and download URL
 $assetName = "zig-wol-$arch-$os.$extension"
 $downloadUrl = "https://github.com/$repo/releases/download/$latestTag/$assetName"
 
-# Define paths
 $homeDir = [System.Environment]::GetFolderPath("UserProfile")
 $installDir = "$homeDir\.zig-wol"
-$tempZip = "$installDir\$assetName"
+$tempTarGz = "$installDir\$assetName"
 
-# If install directory exists already ask user for fresh install
 if (Test-Path $installDir) {
     Write-Host "Existing installation detected in: $installDir"
     $response = Read-Host "Do you want to proceed with a fresh install? Existing configurations will be lost. (y/n)"
@@ -43,34 +35,25 @@ if (Test-Path $installDir) {
     Remove-Item -Recurse -Force $installDir
 }
 
-# Create install directory
 Write-Host "Creating installation directory: $installDir"
 New-Item -ItemType Directory -Path $installDir | Out-Null
 
-# Download the file
 Write-Host "Detected architecture: $arch"
 Write-Host "Downloading latest release ($latestTag) from $downloadUrl..."
-Invoke-WebRequest -Uri $downloadUrl -OutFile $tempZip
+Invoke-WebRequest -Uri $downloadUrl -OutFile $tempTarGz
 
-# Extract the zip file
 Write-Host "Extracting archive..."
-Expand-Archive -Path $tempZip -DestinationPath $installDir -Force
+tar -xzf $tempTarGz -C $installDir
 
-# Clean up zip file
-Remove-Item $tempZip
+Remove-Item $tempTarGz
 
 Write-Host "Installation completed! Files are in: $installDir"
 Write-Host "To use 'zig-wol', consider adding '$installDir' to your PATH."
 
-# Add to PATH but ask user first
 $response = Read-Host "Do you want to add $installDir to your PATH? (y/n)"
 if ($response -eq "y") {
-    # Fetch current user PATH
     $currentPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-    # Check if path is already in PATH
     if ($currentPath -notlike "*$installDir*") {
-        # Append new path
         $newPath = "$currentPath;$installDir"
         [System.Environment]::SetEnvironmentVariable("Path", $newPath, "User")
         Write-Host "Added $installDir to PATH. Restart your terminal or log out/in for changes to take effect."
