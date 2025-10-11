ab00b3c2871d8c59687d3395e3c91e34da13ec38
Riccardo Torreggiani <104195271+rktr1998@users.noreply.github.com>
2025-10-11 20:10:37 +0200

    subcommand status thread-safety (#39)
    * separate ping once and ping many multithread and add mutex
    
    * make subcommand status thread-safe
    
    - implement mutext for thread to lock when writing to ping result array
    - join thread when running status and detach when running status --live
    - adjust console printing accordingly

--- src/ping.zig
+++ src/ping.zig
@@ -1,13 +1,33 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-/// Pings a machine using the system's ping command, returns true if the destination replies.
-pub fn ping_with_os_command(allocator: std.mem.Allocator, alias_fqdn: []const u8, is_alive: *bool) !void {
+/// Pings a machine given a FQDN using the system's ping command in a multithreaded context.
+/// The is_alive pointer is shared between threads, a mutex is used to ensure thread safety.
+/// If ping_forever is true, run indefinitely with a 5 second sleep between pings.
+pub fn ping_with_os_command_multithread(allocator: std.mem.Allocator, alias_fqdn: []const u8, ping_forever: bool, mutex: *std.Thread.Mutex, is_alive: *bool) !void {
+    while (true) {
+        const ping_result = ping_with_os_command(allocator, alias_fqdn) catch |err| {
+            return err;
+        };
+
+        // lock the mutex while updating the shared is_alive variable
+        mutex.lock();
+        is_alive.* = ping_result;
+        mutex.unlock();
+
+        if (!ping_forever) break;
+        std.Thread.sleep(5 * std.time.ns_per_s); // do not spam too many pings if pinging forever
+    }
+}
+
+/// Pings a machine given a FQDN using the system's ping command, returns true if the ping was successful, false otherwise.
+pub fn ping_with_os_command(allocator: std.mem.Allocator, fqdn: []const u8) !bool {
     const args = switch (builtin.target.os.tag) {
-        .linux, .macos => &[_][]const u8{ "ping", "-c", "1", "-W", "1", alias_fqdn },
-        .windows => &[_][]const u8{ "ping", "-n", "1", "-w", "1000", alias_fqdn },
+        .linux, .macos => &[_][]const u8{ "ping", "-c", "1", "-W", "1", fqdn },
+        .windows => &[_][]const u8{ "ping", "-n", "1", "-w", "1000", fqdn },
         else => @compileError("Unsupported OS"),
     };
+
     const result = try std.process.Child.run(.{
         .allocator = allocator,
         .argv = args,
@@ -19,17 +39,18 @@ pub fn ping_with_os_command(allocator: std.mem.Allocator, alias_fqdn: []const u8
         std.mem.indexOf(u8, result.stdout, "unreachable") == null and
         std.mem.indexOf(u8, result.stderr, "unreachable") == null)
     {
-        is_alive.* = true;
+        return true;
     } else {
-        is_alive.* = false;
+        return false;
     }
 }
 
-test "ping_with_os_command works" {
+test "ping_with_os_command" {
     var da = std.heap.DebugAllocator(.{}){};
     defer _ = da.deinit();
     const gpa = da.allocator();
 
-    var is_alive: bool = false;
-    try ping_with_os_command(gpa, "127.0.0.1", &is_alive);
+    try std.testing.expectEqual(true, try ping_with_os_command(gpa, "127.0.0.1"));
+    try std.testing.expectEqual(true, try ping_with_os_command(gpa, "localhost"));
+    try std.testing.expectEqual(false, try ping_with_os_command(gpa, "256.256.256.256"));
 }
